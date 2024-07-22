8bc1c7fd382a1477739246106b1690c37cef4b96
yash3004 <yashsehgal9253@gmail.com>
2024-07-22 21:48:07 +0530

    little_changes

--- backend/flask_app/database.py
+++ backend/flask_app/database.py
@@ -1,6 +1,14 @@
 import sqlalchemy
 from sqlalchemy import String, BIGINT, DATETIME, Column
 
+from sqlalchemy.ext.declarative import declarative_base
 
-class User:
-    Created_at = Column(DATETIME)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user_table"
+    user_id = Column(BIGINT, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String, nullable=False)
+
